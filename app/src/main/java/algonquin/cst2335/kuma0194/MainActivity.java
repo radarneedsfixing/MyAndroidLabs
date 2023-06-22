38ab9e0938035ce3bc2887793acdc297db10e0cf
sharwan kumar <sharwankumar1856@gmail.com>
2023-06-22 02:28:43 -0400

    Finished part

--- app/src/main/java/algonquin/cst2335/kuma0194/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/kuma0194/MainActivity.java
@@ -1,14 +1,45 @@
 package algonquin.cst2335.kuma0194;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Switch;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+
+    ImageView imgView;
+    Switch sw;
+    RotateAnimation rotateAnimation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgView = findViewById(R.id.imageView);
+        sw = findViewById(R.id.spin_switch);
+
+        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
+
+            if (isChecked) {
+                rotateAnimation = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotateAnimation.setDuration(5000);
+                rotateAnimation.setRepeatCount(Animation.INFINITE);
+                rotateAnimation.setInterpolator(new LinearInterpolator());
+                imgView.startAnimation(rotateAnimation);
+            } else {
+                imgView.clearAnimation();
+            }
+
+        });
     }
+
+
 }
 
